5db508ec9167b78a2da6fabc190a83f12d697dbe
Sean Hellum <seanhellum45@gmail.com>
2020-03-11 19:09:44 +0000

    program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gitpod_dotnet_Debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
